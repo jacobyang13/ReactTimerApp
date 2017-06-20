69d1edca377fdde1b750f7272424f0154704fadb
jyy <jyy@jyy-pc.local>
2017-06-20 13:18:45 -0400

    Initial Commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var Nav = require('Nav');
+
+var Main = (props) => {
+  return(
+    <div>
+      <Nav/>
+      <div className = "row">
+        <div className = "columns small-12 medium-6 large-4">
+          <p>Main.jsx Rendered</p>
+          {props.children}
+        </div>
+      </div>
+    </div>
+
+
+  );
+}
+module.exports = Main;
